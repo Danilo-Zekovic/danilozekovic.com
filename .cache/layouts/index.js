f03fc1d5a3493ba71c99593f3a7fc26acbf4aa2d
Danilo-Zekovic <danilozeka93@gmail.com>
2018-06-27 16:35:16 +0200

    rm cash

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/danilo/PROJECTS/portfolio/src/layouts/index.js"
-  import data from "/home/danilo/PROJECTS/portfolio/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
